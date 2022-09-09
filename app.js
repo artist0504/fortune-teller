68d0c3084045afb9a6d763650d76fcb1c28a12f0
artist0504 <cory0504@gmail.com>
2022-09-09 19:51:11 -0400

    added some event listeners

--- app.js
+++ app.js
@@ -2,7 +2,47 @@
 
 // let state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// set event listeners
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
+
+const fortune = document.getElementById('fortune');
+const answers = [
+    'Yes, definitely',
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most Likely',
+    'Outlook good',
+    'Signs point to yes',
+    'Reply hazy, try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Do not count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful',
+];
+const askButton = document.getElementById('ask');
+const resetButton = document.getElementById('reset');
+
+const fortuneContainer = document.getElementById('fortune-container');
+const inputContainer = document.getElementById('input-container');
+
+const fortune = document.getElementById('fortune');
+
+askButton.addEventListener('click', () => {
+    const randNum = Math.floor(Math.random() * answers.length);
+    fortune.textContent = answers[randNum];
+});
+
+resetButton.addEventListener('click', () => {
+    const randNum = Math.floor(Math.random() * answers.length);
+    fortune.textContent = answers[randNum];
+});
